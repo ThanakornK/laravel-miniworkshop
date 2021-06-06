c550a688d5eeede3737970d14a0405037b24058b
ThanakornK <thanakorn.kit@ku.th>
2021-06-06 13:48:59 +0700

    add error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404 Not Found</title>
+</head>
+<body>
+    <div class="container" align="center">
+        <h2>404 Not Found</h2>
+        <h4>Nothing here if you have problem please contact us</h4>
+    </div>
+    
+</body>
+</html>
